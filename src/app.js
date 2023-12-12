c17ead265f0d1b2cb5f0839b42b6f5acc41da853
Mariana <101410421+Placito@users.noreply.github.com>
2023-12-12 17:36:25 +0000

    pcreate the excuse

--- src/app.js
+++ src/app.js
@@ -3,6 +3,24 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = () => {
-  let randomNumber = Math.random() * 10;
-  document.querySelector('^#theExcuse').innerHTML = randomNumber;
+  document.querySelector("#theExcuse").innerHTML = generateExcuse();
+};
+
+//function for the excuses
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["jogger", "racoon", "dog", " driver", "comedian", "pincone"];
+  let action = ["took my", "yelled at my", "stole my", "eat my"];
+  let possetion = ["homework", "toe", "car", "shoe"];
+  let where = ["on the street", "in my house", "in my drive way"];
+
+  // varibles with the random parts of the excuse, you use Math.floor for have an integer number and Math.random() to have mixed index's
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subIndex = Math.floor(Math.random() * subject.length);
+  let acIndex = Math.floor(Math.random() * action.length);
+  let posIndex = Math.floor(Math.random() * possetion.length);
+  let wheIndex = Math.floor(Math.random() * where.length);
+
+  let excuse = `${pronoun[proIndex]} ${subject[subIndex]} ${action[acIndex]} ${possetion[posIndex]} ${where[wheIndex]}`;
+  return excuse;
 };
